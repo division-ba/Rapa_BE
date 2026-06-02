042a17dc2eb5b142e6cecbfcbecc686deccb2fe6
Admin <wlsql852@naver.com>
2026-06-02 10:06:56 +0900

    [CHORE] BaseEntity, ddl-auto 설정 추가

---
+++ src/main/java/io/eddie/unitybe/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package io.eddie.unitybe.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
